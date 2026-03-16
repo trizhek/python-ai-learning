1ef159c3069743efec6fde54c6732c90e841ffb9
Trizhek <trizhek@gmail.com>
2026-03-16 11:34:44 +0400

    Урок 1 - функции, задания 1 и 2

--- lesson1.py
+++ lesson1.py
@@ -1,4 +1,10 @@
 def greeting(name):
-    your_name = input(f"Hello, {name}! You will be an AI developer.")
-    return your_name
-print(greeting("John"))
+    return f"Привет, {name}! Ты будешь ИИ разработчиком."
+    
+
+print(greeting("Артем"))
+
+def add(a, b):
+   return a + b
+
+print(add(5, 3))
